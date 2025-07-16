a2ab6fd463590d388cbc2b47dd063087169c849d
Oleksandr Honchar <figules@gmail.com>
2025-07-16 20:43:35 +0200

    Refactor task-6.ts to include console.log for type-specific function calls and remove redundant calls

--- src/task-6.ts
+++ src/task-6.ts
@@ -6,11 +6,8 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement<number>([1, 2, 3]);           // 1
-getFirstElement<string>(["a", "b", "c"]);     // "a"
-getFirstElement<boolean>([true, false, true]); // true
+console.log(getFirstElement<number>([1, 2, 3]));           // 1
+console.log(getFirstElement<string>(["a", "b", "c"]));     // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
 
 
-console.log(getFirstElement([1, 2, 3]));           
-console.log(getFirstElement(["a", "b", "c"]));     
-console.log(getFirstElement([true, false, true]));
